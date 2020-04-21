3688a4c1fbdbd7f1737a9305daf3aec91683cd99
Simón González <simon.gonzalez.guerra87@gmail.com>
2020-04-21 08:16:43 -0300

    Added simple Java project

---
+++ basicspringbootwebapplication/src/main/java/com/sgonzalez/basicspringbootwebapplication/App.java
@@ -0,0 +1,41 @@
+package com.sgonzalez.basicspringbootwebapplication;
+
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.context.ConfigurableApplicationContext;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+class HelloWorld {
+
+    @RequestMapping("/")
+    public String sayHi() {
+    	return "Hello World!";
+    }
+}
+
+// The scanBasePackageClasses = HelloWorld.class is not really necessary in
+// this case. It is used when the class we are referencing is in a different
+// package than the class we are referencing it from, which is not the case.
+@SpringBootApplication(scanBasePackageClasses = HelloWorld.class)
+public class App {
+	private static Logger logger = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args)  throws Exception {
+	    ConfigurableApplicationContext ctx = SpringApplication.run(App.class, args);
+	    assert (ctx != null);
+	    logger.info("Application started...");
+	    System.in.read();
+	    ctx.close();
+	}
+}
