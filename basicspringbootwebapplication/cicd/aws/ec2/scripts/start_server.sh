eecb6a3d0572ed49bafdfc5926c2d3bc7c5fea83
Simón González <simon.gonzalez.guerra87@gmail.com>
2020-05-04 08:30:52 -0300

    Updated appspec.yml according to buildspec.yml paths

--- basicspringbootwebapplication/cicd/aws/ec2/scripts/start_server.sh
+++ basicspringbootwebapplication/cicd/aws/ec2/scripts/start_server.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-java -jar /basicspringbootwebapplication/basicspringbootwebapplication-0.0.1-SNAPSHOT.jar com.sgonzalez.basicspringbootwebapplication.App
+java -jar /basicspringbootwebapplication/basicspringbootwebapplication-0.0.1-SNAPSHOT.jar com.sgonzalez.basicspringbootwebapplication.App &
