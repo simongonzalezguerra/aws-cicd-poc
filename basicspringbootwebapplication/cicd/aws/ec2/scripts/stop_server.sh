7b4723ce5c78de2ec5da535bb09d77e3128ad430
Simón González <simon.gonzalez.guerra87@gmail.com>
2020-05-06 08:23:55 -0300

    The service stop script checks whether the service is running and doesn't fail if it is not

--- basicspringbootwebapplication/cicd/aws/ec2/scripts/stop_server.sh
+++ basicspringbootwebapplication/cicd/aws/ec2/scripts/stop_server.sh
@@ -1,3 +1,24 @@
 #! /bin/bash
 
-curl -v -X POST localhost:8080/actuator/shutdown
+SERVICE_HOST=localhost
+SERVICE_PORT=8080
+
+nc -z $SERVICE_HOST $SERVICE_PORT
+
+if [ ! $? -eq 0 ]; then
+  echo "Service is not running, nothing to do"
+  exit 0
+fi
+
+echo "Sending shutdown request to service..."
+curl -v -X POST $SERVICE_HOST:$SERVICE_PORT/actuator/shutdown
+curl_result=$?
+echo -ne "\n"
+
+if test "$curl_result" != "0"; then
+  echo "Could not stop service. This may mean that that we couldn't send the shutdown request, or that the shutdown URL is secured. You should check if the service is running and stop it manually"
+  echo "Curl exit code: $curl_result"
+  exit 1
+fi
+
+echo "Service stopped"
